f93ee6204718c4449b7858bb4e07620c9e2872ef
Alan Liu <alnliu2000@gmail.com>
2026-01-24 19:37:50 -0800

    Fix style issues

--- tests/unit/test_data_correction.py
+++ tests/unit/test_data_correction.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
 import pytest
 from csvplus.data_correction import resolve_string_value
+
 
 @pytest.fixture
 def data():
@@ -11,25 +11,30 @@ def data():
     })
     return company_data
 
+
 def test_column_name_error(data):
     """Test that includes a nonexistent column name."""
     with pytest.raises(ValueError, match="The given column_name does not exist."):
         resolve_string_value(data, "store_name", ["Google", "Microsoft"], 80)
+
 
 def test_nonstring_column_error(data):
     """Test that passing a non-string dtype column."""
     with pytest.raises(TypeError, match="The column is not of type string."):
         resolve_string_value(data, "num_searches", ["Google", "Microsoft"], 80)
 
+
 def test_resolved_names_error(data):
     """Test that has an empty resolved_names list."""
     with pytest.raises(ValueError, match="The given resolved_names is empty."):
         resolve_string_value(data, "company_name", [], 80)
 
+
 def test_threshold_value_error(data):
     """Test that includes an out-of-range threshold value."""
     with pytest.raises(ValueError, match="The threshold value is out of range."):
         resolve_string_value(data, "company_name", ["Google", "Microsoft"], -1)
+
 
 @pytest.mark.parametrize(
     "threshold_value, expected_list",
